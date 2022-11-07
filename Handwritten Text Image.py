ee2e951ebc9b2b5d4ad4a00b6953e4dafb3ed679
adeleyeMV <leyeolaniyan@gmail.com>
2022-11-07 12:18:24 +0530

    Create Handwritten Text Image.py

---
+++ Handwritten Text Image.py
@@ -0,0 +1,6 @@
+import pywhatkit
+pywhatkit.text_to_handwriting('''This is text to handwriting
+First of all you will have to install pywhatkit
+It will get installed through pip.
+For more such content you may follow on linkedin https://linkedin.com/in/sudoshivesh/
+or just search sudoshivesh on google''')
