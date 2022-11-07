18a629df6b2260500292b8ada1d0c15cbc2f6961
adeleyeMV <leyeolaniyan@gmail.com>
2022-11-07 11:50:25 +0530

    Create Fake Information Generator.py

---
+++ Fake Information Generator.py
@@ -0,0 +1,8 @@
+Python Fake Information
+from faker import Faker
+fake = Faker()
+print(fake.name())
+print(fake.email())
+print(fake.country())
+
+print(fake.profile())                                                        
