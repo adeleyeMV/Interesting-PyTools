dd5d306c15abbb4002ded406f73728a2c2cc94cd
adeleyeMV <leyeolaniyan@gmail.com>
2022-11-20 11:20:18 +0530

    Create Broken Heart.py

---
+++ Broken Heart.py
@@ -0,0 +1,38 @@
+import turtle
+
+s = turtle.Screen().bgcolor("black")
+t = turtle.Turtle()
+t.speed(3)
+t.width(12)
+
+def curve():
+    for i in range (200):
+        t.right(1)
+        t.forward(1)
+
+def heart():
+    t.color("red","red")
+    t.begin_fill()
+    t.left(140)
+    t.forward(113)
+    curve()
+    t.left(120)
+    curve()
+    t.forward(112)
+    t.end_fill()
+
+heart()
+t.pencolor("black")
+t.penup()
+t.goto(0,170)
+t.pendown()
+
+for broken in range (3):
+    t.left(75)
+    t.forward(40)
+    t.right(65)
+    t.forward(45)
+
+turtle.done()
+
+#This code is contributed by https://linkedin.com/in/sudoshivesh/
